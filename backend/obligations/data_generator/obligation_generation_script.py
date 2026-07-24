0e9b0ae403c5182543c009044c72e92b89ba7834
vridhib <vridhi.brahmbhatt@protonmail.com>
2026-07-24 09:54:22 -0400

    chore: scaffold Django project structure

---
+++ backend/obligations/data_generator/obligation_generation_script.py
@@ -0,0 +1,10 @@
+import random
+import uuid
+from datetime import datetime, timedelta
+import pandas as pd
+from collections import defaultdict
+# generate n participants 
+
+# generate cycles (participants, min and max amount)
+
+# generate obligations (cycles, noise factor, start time)
